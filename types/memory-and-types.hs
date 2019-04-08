7e7b9d2ff20b94c90a122748556f0472b3201147
Josenaldo <josenaldo.asj@gmail.com>
2019-04-08 14:28:14 -0300

    starting implementing the memory

---
+++ types/memory-and-types.hs
@@ -0,0 +1,16 @@
+data TypeValue = TypeValue {tvType :: String, tvValue :: Int }deriving Show 
+data Scope = Scope {scopeScope :: [String]} deriving Show 
+data MemoryCell = MemoryCell {cellName :: String, cellTv :: TypeValue, cellScope :: Scope} deriving Show 
+data Memory = Memory {memCells :: [MemoryCell]} deriving Show 
+
+add_var_to_memory :: Memory -> MemoryCell -> Memory
+add_var_to_memory (Memory (cells)) cell = Memory (cell : cells)
+
+aux_update_var_in_memory :: [MemoryCell] -> MemoryCell -> [MemoryCell]
+aux_update_var_in_memory [] _ = []
+aux_update_var_in_memory (firstCell : cells) newCell
+    | cellName firstCell == cellName newCell = newCell : cells
+    | otherwise = firstCell : aux_update_var_in_memory cells newCell
+
+update_var_in_memory :: Memory -> MemoryCell -> Memory
+update_var_in_memory (Memory cells) cell = Memory (aux_update_var_in_memory cells cell)
