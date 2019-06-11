c0312c18b2c638c0604e507735c50d12aaf47075
Josenaldo <josenaldo.asj@gmail.com>
2019-06-11 00:53:11 -0300

    Started including the new memory in the expressions

--- Expressions/Expressions.hs
+++ Expressions/Expressions.hs
@@ -10,12 +10,24 @@ import Types.Types
 import Text.Parsec
 import Control.Monad.IO.Class
 
+--                                                                              name
+data Type = NatInt | NatBool | NatString | NatDouble | NatSet Type | NatStruct String [(String, Type)] deriving (Show, Eq)
+data Value = ConsNatInt Int | ConsNatBool Bool | ConsNatString String | ConsNatDouble Double | ConsNatSet Type [Value] | ConsNatStruct String [(String, Value)] deriving (Show, Eq)
+
+--                                  id     value
+data Variable = ConstructVariable String Value | ConstructConstantVariable String Value deriving (Show, Eq)
+data Parameter = ConsParameter String Type deriving (Show, Eq)
+
+--                                  id                return                     name
+data Subprogram = ConstructFunction String [Parameter] Type | ConstructProcedure String [Parameter] deriving (Show, Eq)
+data MemoryCell = Variable Variable | Subprogram Subprogram deriving (Show, Eq)
+
 -- General expression
-expression :: ParsecT [Token] [(Token,Token)] IO(Token)     
+expression :: ParsecT [Token] [MemoryCell] IO(Value)     
 expression = exp4
 
 -- Expression that has precedence 4
-exp4 :: ParsecT [Token] [(Token,Token)] IO(Token)
+exp4 :: ParsecT [Token] [MemoryCell] IO(Value)
 exp4 = 
     do
         n1 <- exp3
@@ -23,11 +35,11 @@ exp4 =
         return (result)
 
 -- Evaluates the remainder of a expression that has precedence 4
-eval_remaining_exp4 :: Token -> ParsecT [Token] [(Token,Token)] IO(Token)
+eval_remaining_exp4 :: Value -> ParsecT [Token] [MemoryCell] IO(Value)
 eval_remaining_exp4 n1 = 
     try
     (do
-        op <- bin_num_op_left_4_token
+        op <- bin_op_left_4_token
         n2 <- exp3
         result <- eval_remaining_exp4 (binary_eval n1 op n2)
         return (result))
@@ -36,7 +48,7 @@ eval_remaining_exp4 n1 =
         return (n1))
 
 -- Expression that has precedence 3
-exp3 :: ParsecT [Token] [(Token,Token)] IO(Token)
+exp3 :: ParsecT [Token] [MemoryCell] IO(Value)
 exp3 = 
     do
         n1 <- exp2
@@ -44,11 +56,11 @@ exp3 =
         return (result)
 
 -- Evaluates the remainder of a expression that has precedence 3
-eval_remaining_exp3 :: Token -> ParsecT [Token] [(Token,Token)] IO(Token)
+eval_remaining_exp3 :: Value -> ParsecT [Token] [MemoryCell] IO(Value)
 eval_remaining_exp3 n1 = 
     try
     (do
-        op <- bin_num_op_left_3_token
+        op <- bin_op_left_3_token
         n2 <- exp2
         result <- eval_remaining_exp3 (binary_eval n1 op n2)
         return (result))
@@ -57,7 +69,7 @@ eval_remaining_exp3 n1 =
         return (n1))
 
 -- Expression that has precedence 2
-exp2 :: ParsecT [Token] [(Token,Token)] IO(Token)
+exp2 :: ParsecT [Token] [MemoryCell] IO(Value)
 exp2 = 
     try
     (do
@@ -69,7 +81,7 @@ exp2 =
     exp1
 
 -- Expression that has precedence 1
-exp1 :: ParsecT [Token] [(Token,Token)] IO(Token)
+exp1 :: ParsecT [Token] [MemoryCell] IO(Value)
 exp1 = 
     try
     (do
@@ -80,14 +92,14 @@ exp1 =
     exp0
 
 -- Expression that has precedence 0
-exp0 :: ParsecT [Token] [(Token,Token)] IO(Token)
+exp0 :: ParsecT [Token] [MemoryCell] IO(Value)
 exp0 = try var_attribution <|> exp_const <|> exp_parenthesized <|> exp_local_var
 
 
 
 
 -- Assignment of a value to a variable
-var_attribution :: ParsecT [Token] [(Token,Token)] IO(Token)
+var_attribution :: ParsecT [Token] [MemoryCell] IO(Value)
 var_attribution = do
     a <- id_token
     b <- assignToken
@@ -107,11 +119,11 @@ var_attribution = do
             return (c)
 
 -- Constant expression
-exp_const :: ParsecT [Token] [(Token,Token)] IO(Token)     
+exp_const :: ParsecT [Token] [MemoryCell] IO(Value)     
 exp_const = int_token <|> double_token
 
 -- Parenthesized expression
-exp_parenthesized :: ParsecT [Token] [(Token,Token)] IO(Token)
+exp_parenthesized :: ParsecT [Token] [MemoryCell] IO(Value)
 exp_parenthesized = 
     do
         lparen <- left_paren_token
@@ -120,7 +132,7 @@ exp_parenthesized =
         return (expr)
 
 -- Expression that consists of a local variable
-exp_local_var :: ParsecT [Token] [(Token,Token)] IO(Token)
+exp_local_var :: ParsecT [Token] [MemoryCell] IO(Value)
 exp_local_var = 
     do
         mem <- getState
