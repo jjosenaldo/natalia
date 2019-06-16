10a867b882869f0dc9b15afe045da39db6d46b16
André Winston <a.winston.s97@gmail.com>
2019-06-16 15:18:58 -0300

    Commenting stuff

--- PredefBlocks/Grammar.hs
+++ PredefBlocks/Grammar.hs
@@ -13,17 +13,17 @@ data PredefBlock =
     CONSGlobalsBlock [Initialization]
     deriving (Eq, Show)
 
-data LValue = 
-    CONSLValueId String | -- a = 1;
-    CONSLValueStruct String [String] | -- node.left = 1;
-    CONSLValueArray String [Exp] | -- v[1][2][3] = 1;
-    CONSLValueDerref String Int   -- *ptr = 1;
-    deriving (Eq, Show)
+-- data LValue = 
+--     CONSLValueId String | -- a = 1;
+--     CONSLValueStruct String [String] | -- node.left = 1;
+--     CONSLValueArray String [Exp] | -- v[1][2][3] = 1;
+--     CONSLValueDerref String Int   -- *ptr = 1;
+--     deriving (Eq, Show)
 
-data BinOp = 
-    CONSBinOp Token
-    deriving (Eq, Show)
+-- data BinOp = 
+--     CONSBinOp Token
+--     deriving (Eq, Show)
 
-data UnOp = 
-    CONSUnOp Token
-    deriving (Eq, Show)
+-- data UnOp = 
+--     CONSUnOp Token
+--     deriving (Eq, Show)
