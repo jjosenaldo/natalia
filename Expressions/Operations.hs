d828471a745d198f3ad9434a9c927fc182ed188c
Josenaldo <josenaldo.asj@gmail.com>
2019-06-16 10:23:47 -0300

    Type of unary operators

--- Expressions/Operations.hs
+++ Expressions/Operations.hs
@@ -2,6 +2,7 @@ module Expressions.Operations where
 
 -- natalia's modules
 import Expressions.Grammar
+import Lexical.Lexemes
 import TypeValue.TypeValue
 
 -- Haskell modules
@@ -11,6 +12,19 @@ import Text.Parsec
 getReturnTypeOfBinOp :: BinOp -> Type -> Type -> Type 
 getReturnTypeOfBinOp _ _ _ = NatNothing
 
--- TODO
 getReturnTypeOfUnOp :: UnOp -> Type -> Type 
-getReturnTypeOfUnOp _ _ = NatNothing
+
+getReturnTypeOfUnOp (CONSUnOp (Negation p)) NatBool = 
+    NatBool
+getReturnTypeOfUnOp (CONSUnOp (Negation p)) other = 
+    error ("ERROR at " ++ show(p) ++ ": the ! operator expects a " ++  (getNameOfType NatBool) ++ " but you passed a " ++ (getNameOfType other))
+
+getReturnTypeOfUnOp (CONSUnOp (Minus p)) NatDouble = 
+    NatDouble
+getReturnTypeOfUnOp (CONSUnOp (Minus p)) NatInt = 
+    NatInt
+getReturnTypeOfUnOp (CONSUnOp (Minus p)) other = 
+    error ("ERROR at " ++ show(p) ++ ": the unary - operator expects a " ++  (getNameOfType NatDouble) ++ " or a " ++ (getNameOfType NatInt) ++ " but you passed a " ++ (getNameOfType other))
+
+getReturnTypeOfUnOp (CONSUnOp (Uppersand p)) t =
+    NatPointer t 
