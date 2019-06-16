cbfce566a580a387bd6ad55abb55c87544121165
GabrielArSouza <gabriel_feg@hotmail.com>
2019-06-16 15:06:13 -0300

    implements semantics to literals

--- Expressions/Semantics.hs
+++ Expressions/Semantics.hs
@@ -8,4 +8,11 @@ import TypeValue.TypeValue
 
 -- Haskell modules
 playExpression :: [MemoryCell] -> Exp -> ([MemoryCell], Value)
+
+-- semantics to literals
 playExpression memory (CONSExpLit t (Int x p)) = (memory, ConsNatInt x)
+playExpression memory (CONSExpLit t (Double x p)) = (memory, ConsNatDouble x)
+playExpression memory (CONSExpLit t (String x p)) = (memory, ConsNatString x)
+playExpression memory (CONSExpLit t (Bool x p)) = (memory, ConsNatBool x)
+playExpression memory (CONSExpLit t (Null p)) = (memory, ConsNatNull )
+
